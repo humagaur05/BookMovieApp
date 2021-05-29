d9e7a5f4e2b97d777066d1c6eb2514bede9df013
Huma <humagaur05@gmail.com>
2021-05-29 21:39:20 +0530

    add Controller.js first version

---
+++ src/screens/Controller.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Home from './home/Home.js'
+
+
+
+
+export default function Controller(props) {
+    const baseUrl = "http://localhost:8085/api/v1"
+    return(
+        <Router>
+           
+        </Router>
+            
+    )
+}
